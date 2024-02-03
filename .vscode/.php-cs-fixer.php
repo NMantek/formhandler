b39ca43cd31d9d493e1fedcf6e1f410c8ef63ad2
Thomas Lüder <t.lueder@nets.de>
2024-02-03 23:59:59 +0800

    Update dependencies and devDependencies

--- .vscode/.php-cs-fixer.php
+++ .vscode/.php-cs-fixer.php
@@ -1,16 +1,24 @@
 <?php
 
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
 // liste der Regeln: https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/rules/index.rst
 
-$finder = PhpCsFixer\Finder::create()
+$finder = Finder::create()
   ->exclude('vendor')
   ->exclude('Vendor')
   ->ignoreVCSIgnored(true)
 ;
 
-return (new PhpCsFixer\Config())
+return (new Config())
   ->setRules([
     '@PhpCsFixer' => true,
+    'curly_braces_position' => [
+      'control_structures_opening_brace' => 'same_line',
+      'classes_opening_brace' => 'same_line',
+      'functions_opening_brace' => 'same_line',
+    ],
     'braces' => [
       'position_after_functions_and_oop_constructs' => 'same',
     ],
