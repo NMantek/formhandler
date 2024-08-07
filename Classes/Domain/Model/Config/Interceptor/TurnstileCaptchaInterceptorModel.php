a9ee36e2be4b6f1fc7ac99c26cf3bb5312af73c4
Nicklas Mantek <mantek@jakota.de>
2024-08-07 12:39:45 +0200

    add turnstile captcha interceptor

---
+++ Classes/Domain/Model/Config/Interceptor/TurnstileCaptchaInterceptorModel.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of TYPO3 CMS-based extension "Formhandler" by JAKOTA.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ */
+
+namespace Typoheads\Formhandler\Domain\Model\Config\Interceptor;
+
+use Typoheads\Formhandler\Interceptor\TurnstileCaptchaInterceptor;
+
+class TurnstileCaptchaInterceptorModel extends AbstractInterceptorModel {
+  /**
+   * @param array<string, mixed> $settings
+   */
+  public function __construct(array $settings) {}
+
+  public function class(): string {
+    return TurnstileCaptchaInterceptor::class;
+  }
+}
---
+++ Classes/Interceptor/TurnstileCaptchaInterceptor.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of TYPO3 CMS-based extension "Formhandler" by JAKOTA.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ */
+
+namespace Typoheads\Formhandler\Interceptor;
+
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use Typoheads\Formhandler\Definitions\Severity;
+use Typoheads\Formhandler\Domain\Model\Config\FormModel;
+use Typoheads\Formhandler\Domain\Model\Config\Interceptor\AbstractInterceptorModel;
+use Typoheads\Formhandler\Domain\Model\Config\Interceptor\TurnstileCaptchaInterceptorModel;
+use Typoheads\Formhandler\Http\TurnstileVerifyRequester;
+
+class TurnstileCaptchaInterceptor extends AbstractInterceptor {
+  public function __construct(
+    private TurnstileVerifyRequester $requestClient
+  ) {}
+
+  public function process(FormModel &$formConfig, AbstractInterceptorModel &$interceptorConfig): bool {
+    if (!$interceptorConfig instanceof TurnstileCaptchaInterceptorModel) {
+      return false;
+    }
+
+    $extensionConfiguration = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('formhandler');
+
+    $turnstilePrivateKey = isset($extensionConfiguration['turnstile']['privatekey']) ? $extensionConfiguration['turnstile']['privatekey'] : '';
+    $turnstileWidgetToken = isset($formConfig->captchaFieldValues['turnstile']) ? $formConfig->captchaFieldValues['turnstile'] : '';
+
+    try {
+      $validationResult = $this->requestClient->request($turnstilePrivateKey, $turnstileWidgetToken, GeneralUtility::getIndpEnv('REMOTE_ADDR'));
+
+      if (isset($validationResult['success']) && true === $validationResult['success']) {
+        return true;
+      }
+
+      $formConfig->formErrors[] = 'captcha_failed';
+
+      return false;
+    } catch (\Exception $error) {
+      $formConfig->formErrors[] = 'captcha_failed';
+      $formConfig->debugMessage('turnstile_captcha_failed', [$error->getMessage()], Severity::Error);
+
+      return false;
+    }
+  }
+}
